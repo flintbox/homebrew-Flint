1ff2f0dd94a219b2ae47ba56a4aadbfcd3f3c693
Jason Nam <contact@jasonnam.com>
2018-06-06 18:39:47 +0700

    Update version to 0.1.1

--- Formula/flint.rb
+++ Formula/flint.rb
@@ -1,8 +1,8 @@
 class Flint < Formula
     desc "Generate projects and files from template."
     homepage "https://github.com/flintbox/Flint"
-    url "https://github.com/flintbox/Flint/archive/0.1.0.tar.gz"
-    sha256 "858bfb9f18a67e3051f24d740b41341421a654c5ec15e40d23c4d7f178b93d85"
+    url "https://github.com/flintbox/Flint/archive/0.1.1.tar.gz"
+    sha256 "ca595823c45abc3e247ca9562b1293c7d8515a58c11817b17921ab35e34ec95a"
 
     def install
         # fixes an issue an issue in homebrew when both Xcode 9.3+ and command line tools are installed
